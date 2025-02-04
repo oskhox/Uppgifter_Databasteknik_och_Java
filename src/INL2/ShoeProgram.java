2ac25421c8c6c8ad8ce6fc1a354787243fff71f6
Oskar Høxbroe <oskar.hoxbroe@gmail.com>
2025-02-04 12:51:03 +0100

    småfix

--- src/INL2/ShoeProgram.java
+++ src/INL2/ShoeProgram.java
@@ -1,10 +1,5 @@
 package INL2;
 
-//Java-koden ska vara prydligt objektorienterad. Detta betyder att du ska ha klasser som
-//motsvarar de tabeller i databasen som du interagerar med. När du läser från tabellerna
-//skapar du upp objekt av motsvarande klass, i Java, och låter inte lösryckta data från
-//databasen åka runt i programmet.
-
 import java.io.FileInputStream;
 import java.sql.*;
 import java.util.ArrayList;
